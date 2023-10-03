8e319c860ddb85dd67cd7835fa028dc897e1e417
alfbrnsy <aldofebriansyah0224@gmail.com>
2023-10-03 07:49:50 +0700

    aldo

---
+++ contohvariable03.java
@@ -0,0 +1,19 @@
+/**
+ * contohvariable03
+ */
+public class contohvariable03 {
+
+    public static void main(String[] args) {
+        String salahsatuhobbysayaadalah = "lari sore";
+        boolean isPandai = true;
+        char jeniskelamin = 'L';
+        byte _umursayasekarang = 20;
+        double $ipk = 4.00, tinggi = 178;
+        System.out.println(salahsatuhobbysayaadalah);
+        System.out.println("apakah pandai? " + isPandai);
+        System.out.println("jenis kelamin: " + jeniskelamin);
+        System.out.println("umurku saat ini: " + _umursayasekarang);
+        System.out.println(String.format("saya beripk 4.00, dengan tinggi badan 178, $ipk, tinggi"));
+        
+    }
+}
